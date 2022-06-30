75ee1145a2b6e4e949008c09e94baa98c861ed74
Zeebrow <mzborowski@yahoo.com>
2022-06-30 16:34:49 -0400

    test workflow

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup, find_packages
+from pathlib import Path
 
-print(f"setup.py=====> {find_packages(where='src/quickhost')}")
+print(f"setup.py=====> {find_packages(where=str(Path('src/quickhost')))}")
 setup(
     name='quickhost',
     version='0.0.1',
-    package_dir={'':'src'},
-    packages=find_packages(where='src/quickhost'),
+    package_dir={'':str(Path('src'))},
+    packages=find_packages(where=str(Path('src/quickhost')))),
     install_requires=[
         'boto3'
     ],
-    scripts=['src/scripts/main.py']
+    scripts=[str(Path('src/scripts/main.py'))]
 )
