09c5cdcb0bfd3a4716a058214af00fe3cdcc97bf
Zeebrow <mzborowski@yahoo.com>
2022-10-19 15:45:24 -0400

    add licence

--- src/scripts/main.py
+++ src/scripts/main.py
@@ -1,3 +1,18 @@
+# Copyright (C) 2022 zeebrow
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import json
 import argparse
 import sys
@@ -35,6 +50,15 @@ def do_logging():
     sh.setFormatter(logging.Formatter(debug_fmt))
     #sh.setFormatter(logging.Formatter(just_text))
     logger.addHandler(sh)
+
+def show_about():
+    # TODO: cli flag
+    print("""
+<program>  Copyright (C) <year>  <name of author>
+This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
+This is free software, and you are welcome to redistribute it
+under certain conditions; type `show c' for details.
+    """)
 
 def cli_main():
 #######################################################################################
